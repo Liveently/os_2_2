2d33414ca4f9bbc50d7eec716d1fff197746da2b
Marina Kosolap <70973716+Liveently@users.noreply.github.com>
2023-04-17 09:21:46 +0700

    Update no_syscall.c

--- no_syscall.c
+++ no_syscall.c
@@ -1,12 +1,13 @@
 #include <sys/syscall.h>
 #include <stdlib.h>
 
-#define STRING_SIZE 14
 #define ERROR  (-1)
 #define STDOUT  1
 
-int main() {
 
+
+ssize_t my_write(int fileDescriptor, const void* buffer, size_t countOfBytes) {
+    
     register int rdi          asm("rdi") = STDOUT;
     register char *rsi        asm("rsi") = "Hello, world!\n";
     register int rax          asm("rax") = SYS_write;
@@ -15,7 +16,17 @@ int main() {
     asm("syscall");                                                        
     
     register ssize_t ret_value   asm("rax");
+    
+    return ret_value;
+}
 
-    return (ret_value == ERROR) ? EXIT_FAILURE : EXIT_SUCCESS;
+int main() {
+
+    size_t length = 14;
+    char* message = "Hello, world!\n";
+
+    ssize_t ret = my_write(STDOUT, message, length);
+    
+    return (ret == ERROR) ? EXIT_FAILURE : EXIT_SUCCESS;
 
 }
